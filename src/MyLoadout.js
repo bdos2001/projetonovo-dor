3cfb2a09c9cdeb674b1cb66bfe168cf7d86f7f61
bdos2001 <bdos@ua.pt>
2024-12-10 16:25:44 +0000

    assadsa

--- src/MyLoadout.js
+++ src/MyLoadout.js
@@ -19,7 +19,6 @@ const MyLoadout = () => {
                 // Filtra as skins
                 const Glock18 = data.filter((skin) => skin.weapon.name === "Glock-18");
                 setGlock(Glock18);
-
                 const USPs = data.filter((skin) => skin.weapon.name === "USP-S");
                 setUSPs(USPs);
 
@@ -44,55 +43,51 @@ const MyLoadout = () => {
 
     return (
         <>
-            <h1>My Loadout</h1>
-            <div className="containerToggle">
-                <input id="check" name="check" type="checkbox" />
-                <label className="toggle" htmlFor="check">
-                    <div className="toggle__circle"></div>
-                </label>
+            <body className="corpo">
+                <div className="containerToggle">
+                    <input id="check" name="check" type="checkbox" />
+                    <label className="toggle" htmlFor="check">
+                        <div className="toggle__circle"></div>
+                    </label>
+                </div>
 
-                <div className="toggle-text">
-                    <span>T</span>
-                    <span>CT</span>
+                <div className="Galeria">
+                    <div className="ItemDaGaleria">
+                        <img src={ImagemGlock} alt="Selected Glock" />
+                        <select
+                            onChange={(e) => {
+                                const selectedGlock = Glock.find(glock => glock.name === e.target.value);
+                                if (selectedGlock) {
+                                    setImagemGlock(selectedGlock.image);
+                                }
+                            }}
+                        >
+                            {Glock.map((glock18) => (
+                                <option key={glock18.name} value={glock18.name}>
+                                    {glock18.name}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+                    <div className="ItemDaGaleria" >
+                        <img src={ImagemUSPs} alt="Selected USP-S" />
+                        <select
+                            onChange={(e) => {
+                                const selectedUSPs = USPs.find(usps => usps.name === e.target.value);
+                                if (selectedUSPs) {
+                                    setImagemUSPs(selectedUSPs.image);
+                                }
+                            }}
+                        >
+                            {USPs.map((usps) => (
+                                <option key={usps.name} value={usps.name}>
+                                    {usps.name}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
                 </div>
-            </div>
-
-            <div className="Galeria">
-                <div className="ItemDaGaleria">
-                    <img src={ImagemGlock} alt="Selected Glock" />
-                    <select
-                        onChange={(e) => {
-                            const selectedGlock = Glock.find(glock => glock.name === e.target.value);
-                            if (selectedGlock) {
-                                setImagemGlock(selectedGlock.image);
-                            }
-                        }}
-                    >
-                        {Glock.map((glock18) => (
-                            <option key={glock18.name} value={glock18.name}>
-                                {glock18.name}
-                            </option>
-                        ))}
-                    </select>
-                </div>
-                <div className="ItemDaGaleria">
-                    <img src={ImagemUSPs} alt="Selected USP-S" />
-                    <select
-                        onChange={(e) => {
-                            const selectedUSPs = USPs.find(usps => usps.name === e.target.value);
-                            if (selectedUSPs) {
-                                setImagemUSPs(selectedUSPs.image);
-                            }
-                        }}
-                    >
-                        {USPs.map((usps) => (
-                            <option key={usps.name} value={usps.name}>
-                                {usps.name}
-                            </option>
-                        ))}
-                    </select>
-                </div>
-            </div>
+            </body>
         </>
     );
 };
